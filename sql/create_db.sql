89202d46cb2a9f43e48d3ea234bd4f8d2f7117eb
alexsosat <56005779+alexsosat@users.noreply.github.com>
2021-03-17 14:36:05 -0600

    changes in database
    add initial testing values

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -4,19 +4,19 @@ use advAssets;
 
 CREATE TABLE `User`(
     `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `surname` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `user_image` BLOB NULL
+    `name` VARCHAR(20) NOT NULL,
+    `surname` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    `user_image` LONGBLOB NULL
 );
 
 CREATE TABLE `Publication`(
     `id_pub` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user_id` INT UNSIGNED NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `desc` VARCHAR(255) NULL,
-    `url` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `desc` VARCHAR(500) NULL,
+    `url` VARCHAR(100) NOT NULL,
     `dimension` INT NOT NULL,
     `format` INT NOT NULL,
     FOREIGN KEY (`user_id`)
@@ -27,10 +27,11 @@ CREATE TABLE `Publication`(
 CREATE TABLE `Image`(
     `id_img` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `pub_id` INT UNSIGNED NOT NULL,
-    `image_file` BLOB NOT NULL,
+    `image_file` LONGBLOB NOT NULL,
     FOREIGN KEY (`pub_id`)
         REFERENCES `Publication` (`id_pub`)
         ON DELETE CASCADE
 );
+
 
 
