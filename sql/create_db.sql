3cd71167a87be9c0413425d00071874d1ac7ae8d
alexsosat <56005779+alexsosat@users.noreply.github.com>
2021-03-24 15:04:55 -0600

    Database changes and surname added to controller

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -3,16 +3,16 @@ create database advAssets;
 use advAssets;
 
 CREATE TABLE `Users`(
-    `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(20) NOT NULL,
     `surname` VARCHAR(50) NOT NULL,
     `email` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(180) NOT NULL,
     `user_image` LONGBLOB NULL
 );
 
 CREATE TABLE `Publication`(
-    `id_pub` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user_id` INT UNSIGNED NOT NULL,
     `title` VARCHAR(100) NOT NULL,
     `desc` VARCHAR(500) NULL,
@@ -20,21 +20,21 @@ CREATE TABLE `Publication`(
     `dimension` INT NOT NULL,
     `format` INT NOT NULL,
     FOREIGN KEY (`user_id`)
-        REFERENCES `User` (`id_user`)
+        REFERENCES `Users` (`id`)
         ON DELETE CASCADE
 );
 
 CREATE TABLE `Image`(
-    `id_img` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `pub_id` INT UNSIGNED NOT NULL,
     `image_file` LONGBLOB NOT NULL,
     FOREIGN KEY (`pub_id`)
-        REFERENCES `Publication` (`id_pub`)
+        REFERENCES `Publication` (`id`)
         ON DELETE CASCADE
 );
 
 
-ALTER TABLE `user` ADD `created_at` TIMESTAMP NULL AFTER `user_image`, ADD `updated_at` TIMESTAMP NULL AFTER `created_at`, ADD `deleted_at` TIMESTAMP NULL AFTER `updated_at`;
+ALTER TABLE `users` ADD `created_at` TIMESTAMP NULL AFTER `user_image`, ADD `updated_at` TIMESTAMP NULL AFTER `created_at`, ADD `deleted_at` TIMESTAMP NULL AFTER `updated_at`;
 
 ALTER TABLE `publication` ADD `created_at` TIMESTAMP NULL AFTER `format`, ADD `updated_at` TIMESTAMP NULL AFTER `created_at`, ADD `deleted_at` TIMESTAMP NULL AFTER `updated_at`;
 
